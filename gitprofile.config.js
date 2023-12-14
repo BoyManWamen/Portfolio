ccfc7faedd7de367956081e79ade9070dfcb37f0
Huy Hoang <71991756+BoyManWamen@users.noreply.github.com>
2023-12-13 16:06:05 -0800

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -62,7 +62,7 @@ const config = {
   ], */
   education: [
     {
-      institution: 'University of California, Merced',
+      institution: 'University of California, Davis',
       degree: 'Computer Science and Engineering, B.S.',
       from: '2023',
       to: '2027',
